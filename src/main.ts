e27ab0c1cd21e710de8f4754de334cdd1c19aa4e
462441355@qq.com <462441355@qq.com>
2023-06-20 16:52:46 +0800

    * 先提交开始忙了

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueLazyLoad from "vue3-lazyload";
+import ViewUiPlus from "view-ui-plus";
+import "view-ui-plus/dist/styles/viewuiplus.css";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueLazyLoad, {})
+  .use(ViewUiPlus)
+  .mount("#app");
