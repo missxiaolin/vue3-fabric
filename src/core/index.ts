342ef2c6d2aa9d677daacfee628dbdc25effb7e4
462441355@qq.com <462441355@qq.com>
2023-07-04 18:24:06 +0800

    * 复制

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "events";
 import type EditorWorkspace from "./EditorWorkspace";
+import { v4 as uuid } from 'uuid';
 
 import initControls from './initControls';
 import InitCenterAlign from './initCenterAlign';
@@ -20,6 +21,41 @@ class Editor extends EventEmitter {
     // 初始化canvas
     initControls(canvas);
     this.centerAlign = new InitCenterAlign(canvas);
+  }
+
+  /**
+   * 单个对象复制
+   * @param activeObject 
+   */
+  _copyObject(activeObject: fabric.Object) {
+    // 间距设置
+    const grid = 10;
+    const canvas = this.canvas;
+    activeObject?.clone((cloned: fabric.Object) => {
+      if (cloned.left === undefined || cloned.top === undefined) return;
+      canvas.discardActiveObject();
+      // 设置位置信息
+      cloned.set({
+        left: cloned.left + grid,
+        top: cloned.top + grid,
+        evented: true,
+        id: uuid(),
+      });
+      canvas.add(cloned);
+      canvas.setActiveObject(cloned);
+      canvas.requestRenderAll();
+    });
+  }
+
+  /**
+   * 复制
+   * @param paramsActiveObeject 
+   * @returns 
+   */
+  clone(paramsActiveObeject: fabric.ActiveSelection | fabric.Object) {
+    const activeObject = paramsActiveObeject || this.canvas.getActiveObject();
+    if (!activeObject) return;
+    this._copyObject(activeObject);
   }
 }
 
