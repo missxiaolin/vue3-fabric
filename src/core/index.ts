85f4c611a2d19e5c3a97604d52264d96db5612c4
462441355@qq.com <462441355@qq.com>
2023-07-04 11:16:53 +0800

    * 初始化

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,8 @@
 import { EventEmitter } from "events";
 import type EditorWorkspace from "./EditorWorkspace";
 
+import initControls from './initControls';
+
 class Editor extends EventEmitter {
   canvas: fabric.Canvas;
   editorWorkspace: EditorWorkspace | null;
@@ -8,6 +10,9 @@ class Editor extends EventEmitter {
     super();
     this.canvas = canvas;
     this.editorWorkspace = null;
+
+    // 初始化canvas
+    initControls(canvas);
   }
 }
 
