511741f99c47056d6aa018afaff806355f10e602
lingongbing <462441355@qq.com>
2024-02-19 13:51:42 +0800

    * 解决文字导出不清楚的问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -209,6 +209,7 @@ onMounted(() => {
     fireRightClick: true, // 启用右键，button的数字为3
     stopContextMenu: true, // 禁止默认右键菜单
     controlsAboveOverlay: true, // 超出clipPath后仍然展示控制条
+    imageSmoothingEnabled: false, // 解决文字导出后不清晰问题
   });
   // 初始化编辑器
   canvasEditor.init(canvas);
